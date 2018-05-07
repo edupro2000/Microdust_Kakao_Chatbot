255931d64d74e6af7818971e66372e89ac72e80f
Ubuntu <ubuntu@ip-172-31-5-60.ap-northeast-2.compute.internal>
2018-05-07 22:48:23 +0900

    arrangement

--- chatbot/Total_Functions.py
+++ chatbot/Total_Functions.py
@@ -3,15 +3,18 @@
 
 # ## 한국환경공단 대기오염 API를 이용한 미세먼지 데이터 가져오기
 
-# In[2]:
+# In[18]:
 
 
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import googlemaps
+
+from gensim.models import Word2Vec
 
 
-# In[3]:
+# In[19]:
 
 
 #미세먼지 및 초미세먼지 농도별 등급 판정
@@ -55,21 +58,28 @@ def nano_grade(num) :
 # - 환경공단의 API가 인지하지 못하는 지역위치 값 입력시
 # > 구글 API로 위치 재조정
 
-# In[8]:
+# In[20]:
 
 
-with open('secret/get_tm_coor_key.txt', 'rb') as f :
+with open('microdust/secret/get_tm_coor_key.txt', 'rb') as f :
     get_tm_coor_key = pickle.load(f)
 
 
-# In[32]:
+# In[21]:
 
 
-with open("secret/google_secret_key.txt", "rb") as f :
+with open("microdust/secret/google_secret_key.txt", "rb") as f :
     google_secret_key = pickle.load(f)
 
 
-# In[53]:
+# In[22]:
+
+def check_detail(loc) : 
+    url = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getTMStdrCrdnt?umdName="+ loc +"&pageNo=1&numOfRows=10&ServiceKey=" + get_tm_coor_key
+    response = requests.get(url)
+    dom = BeautifulSoup(response.content, "html.parser")
+    name_ls = [name.text + " " + loc for name in dom.select("sidoname")]
+    return name_ls
 
 
 def get_tm_coor(loc) : 
@@ -87,10 +97,8 @@ def get_tm_coor(loc) :
             X, Y = get_tm_coor(loc)
             result = X, Y
         except : 
-            result = "요청하신 지역에 오타가 있는지 확인해주세요!"
-        
+            result = "error"
     else : 
-        
         X = dom.select("tmx")[0].text
         Y = dom.select("tmy")[0].text
         result = X, Y
@@ -100,36 +108,19 @@ def get_tm_coor(loc) :
 
 # -----------
 
-# ## 1-1. 구글 API를 이용한 지역명 조정
-# - 한국 환경공단 대기오염 API가 인지하지 못하는 지역명을 조정표
-
-# In[24]:
-
-
-import googlemaps
-def get_good_name(place) : 
-    try : 
-        gmaps = googlemaps.Client(key = google_secret_key)
-        place_information = gmaps.geocode(place, language = 'ko')
-        result = place_information[0]['address_components'][1]['long_name']
-    except : 
-        result = "요청하신 지역에 오타가 있는지 확인해주세요!"
-    return result
-
-
 # ## TM 기준좌표 조회
 # - 검색서비스를 사용하여 읍면동 이름을 검색조건으로 기준좌표 (TM좌표)정보를 제공하는 서비스
 # - 입력값으로는 TM좌표를 입력
 # * TM 좌표는, 위경도와 형태는 비슷하나 내용은 다른 위치 표기법
 
-# In[12]:
+# In[23]:
 
 
-with open("secret/tm_station_key.txt", "rb") as f :
+with open("microdust/secret/tm_station_key.txt", "rb") as f :
     tm_station_key = pickle.load(f)
 
 
-# In[13]:
+# In[24]:
 
 
 def nearest_station(X, Y) : 
@@ -141,24 +132,12 @@ def nearest_station(X, Y) :
     return name, distance + "km"
 
 
-# In[14]:
-
-
-X, Y = get_tm_coor("병점동")
-
-
-# In[15]:
-
-
-nearest_station(X, Y)
-
-
 # --------
 
 # ## 실시간 미세 먼지 조회
 # - 위치를 입력받아 실제로 미세먼지를 결과값으로 도출하는 함수
 
-# In[22]:
+# In[25]:
 
 
 def microdust_1(loc) : 
@@ -168,31 +147,51 @@ def microdust_1(loc) :
     date = dom.select("datatime")[0].text
     pm10 = dom.select("pm10value")[0].text
     pm25 = dom.select("pm25value")[0].text
-    result = '''{}에 관측했을 때, 
+    result = '''
+    
+    {}에 관측했을 때, 
     미세먼지 농도 : {}({}), 
-    초 미세먼지 농도 : {}({})였습니다!'''.format(date, pm10, grade(pm10),  pm25, nano_grade(pm25))
+    초 미세먼지 농도 : {}({})였습니다!
+    
+    선택하신 지역의 인접지역 미세먼지 정보는 버튼을 통해 확인하실 수 있습니다.
+    
+    '''.format(date, pm10, grade(pm10),  pm25, nano_grade(pm25))
     return result
+
+
+# In[73]:
+
+
+def near_area(loc) : 
+    try : 
+        output_ls = []
+        model = Word2Vec.load('model')
+        near_area = model.wv.most_similar(loc, topn = 3)
+        for name, similarity in near_area :
+            if float(similarity) >= 0.9 :
+                output_ls.append([name, similarity])
+            
+        output_ls = [name for name, similarity in output_ls]
+        
+    except :
+        
+        output_ls = ["개발중인", "지역입니다.", "죄송합니다"]
+    
+    return output_ls
 
 
 # ## 최종 함수
 # - 알고 싶은 위치에서 가장 가까운 관측소에서 측정한 미세먼지 및 초미세먼지 농도를 출력
 
-# In[19]:
+# In[27]:
 
 
 def get_microdust(place) : 
     try : 
         X, Y = get_tm_coor(place)
-    except :
-        get_good_name 
         name, distance = nearest_station(X, Y)
         result = microdust_1(name)
-        
-    return result, name, distance
-
-
-# In[59]:
-
-
-get_microdust("")
+    except : 
+        result = "오타가 있는지 확인해주세요!(대화 기능은 구현 중입니다)"
+    return result
 
--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,11 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
 
-#---------------------------------
-# quizbot.py
-#---------------------------------
-
-
-
 import os
 from flask import Flask, request, jsonify
 from Total_Functions import *
@@ -21,7 +15,7 @@ def Keyboard():
 
     dataSend = {
         "type" : "buttons",
-        "buttons" : [u"미세먼지"]
+        "buttons" : ["미세먼지를 알려줘"]
     }
 
     return jsonify(dataSend)
@@ -29,21 +23,50 @@ def Keyboard():
 
 @app.route('/message', methods=['POST'])
 def Message():
-
     dataReceive = request.get_json()
     content = dataReceive['content']
     user_key = dataReceive['user_key']
     save_messages(user_key, content)
+    if content == "미세먼지를 알려줘" or content == "다른 지역 미세먼지를 알려줘" :
 
-    if content == "미세먼지" :
-
-        dataSend = {"message": {"text": "사는 지역을 알려주시면 실시간으로 미세먼지를 알려드려요!"}}
+        dataSend = {
+            "message":
+            {
+            "text": '''지역명만 알려주시면 실시간으로 미세먼지를 알려드려요! 대화형 챗봇이 아닙니다! 
+       상도역, 강남 교보문고 처럼 입력해주세요!'''
+            }
+                   }
 
     else :
+        list_of_places = check_detail(content)
+        place = get_microdust(content)
+        if len(list_of_places) > 1 :
+            dataSend = {
+                "message" :
+                {
+                    "text" : "중복되는 지명입니다! 어느 곳인지 지정해주세요!"
+                },
+                "keyboard" : {
+                    "type" : "buttons",
+                    "buttons" : list_of_places,
+                }
+            }
+        elif "오타" not in place: 
+            dataSend = {
+                "message" : 
+                {
+                    "text" : place
+                }
+            }
+        else : 
+            dataSend = {
+                "message" : 
+                {
+                    "text" : "다시 입력해주세요."
+                }
+            }
 
-        dataSend = {"message": {"text": get_microdust(content) } }
-
-    return jsonify(dataSend)
+    return jsonify(dataSend) 
 
 
 
